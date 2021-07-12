33b868d10d08436b3923af424ef18b9aef12ae43
Kimura <mkimura7878@gmail.com>
2021-07-13 00:52:52 +0900

    ItemServiceを作成した

---
+++ app/src/main/java/com/example/qiitaretrofit/network/ItemService.kt
@@ -0,0 +1,30 @@
+package com.example.qiitaretrofit.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://qiita.com/api/v2/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ItemService {
+    @GET("items")
+    fun getProperties(): Call<String>
+}
+
+object ItemApi {
+    val retrofitService :ItemService by lazy {
+        retrofit.create(ItemService::class.java) }
+}
