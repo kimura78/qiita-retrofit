8a4aaa745108e0ba2f23dd7b6cc19c7ca76891ca
Kimura <mkimura7878@gmail.com>
2021-07-14 20:29:13 +0900

    install okhttp cardview

--- app/src/main/java/com/example/qiitaretrofit/network/ItemService.kt
+++ app/src/main/java/com/example/qiitaretrofit/network/ItemService.kt
@@ -2,23 +2,22 @@ package com.example.qiitaretrofit.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://qiita.com/api/v2/ "
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
+private val okHttpClient = OkHttpClient.Builder().build()
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(okHttpClient)
     .build()
 
 interface ItemApiService {
-
     @GET("items")
     suspend fun getProperties(): List<ItemProperty>
 }
